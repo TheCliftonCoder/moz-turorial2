cdc331cdf7654e138e833a6d70d8674433738c8c
jat92 <jat92@aber.ac.uk>
2026-02-04 14:49:41 +0000

    Completed functionality for the app

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,30 @@ import Todo from "./components/Todo";
 import { useState } from "react";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP)
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
 
   function editTask(id, newName) {
-    const editedTask = tasks.map((task) => {
+    const editedTaskList = tasks.map((task) => {
       // if this task has the same ID as the edited task
       if (id === task.id) {
-        // copy the task and update its name
-        return { ...task, name: newName};
+        // Copy the task and update its name
+        return { ...task, name: newName };
       }
-      // return the orginal task if it's not the edited task
+      // Return the original task if it's not the edited task
       return task;
     });
     setTasks(editedTaskList);
-  }
+}
 
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map((task) => {
@@ -38,15 +47,26 @@ function App(props) {
     setTasks(remainingTasks);
   }
 
-  const taskList = tasks?.map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-      editTask={editTask}
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
   ));
 
@@ -64,11 +84,7 @@ function App(props) {
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
-      <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
+      <div className="filters btn-group stack-exception">{filterList}</div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
